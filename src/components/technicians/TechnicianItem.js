9801458dee895e14d29f9d97b477026075e2f466
Karen Okonkwo <karenokonkwo29@gmail.com>
2020-08-06 17:14:30 +0100

    show toast on delete

--- src/components/technicians/TechnicianItem.js
+++ src/components/technicians/TechnicianItem.js
@@ -1,13 +1,18 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
+import M from "materialize-css//dist/js/materialize.min.js";
 import { deleteTechnician } from "../../redux/actions/technicianActions";
 
 const TechnicianItem = ({ technician, deleteTechnician }) => {
+	const onDelete = () => {
+		deleteTechnician(technician.id);
+		M.toast({ html: `Technician deleted.` });
+	};
 	return (
 		<li className="collection-item">
 			{technician.first_name} {technician.last_name}
-			<a href="#!" onClick={() => deleteTechnician(technician.id)} className="secondary-content">
+			<a href="#!" onClick={onDelete} className="secondary-content">
 				<i className="material-icons red-text text-darken-2">delete</i>
 			</a>
 		</li>
